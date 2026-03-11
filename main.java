7e787c93396f4949ac583a35b19746186729c81b
ManguAnanya <mangu.ananya@gmail.com>
2026-03-11 10:02:26 +0530

    features updated

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ package InSemLabFSAD;
 public class main {
     public static void main(String[] args) {
         System.out.println("FSAD Lab Program Running");
+        System.out.println("Features updated");
     }
 }
