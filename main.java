f4ea81c9b33fac6af0692cedcebb5b396bacc20e
ManguAnanya <mangu.ananya@gmail.com>
2026-03-11 10:03:48 +0530

    bugs fixed

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("FSAD Lab Program Running");
         System.out.println("Features updated");
+        System.out.println("Bugs fixed");
     }
 }
